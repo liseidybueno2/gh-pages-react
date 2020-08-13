a510755f7ce49a09e73afe1be192b6fd440d982b
Liseidy Bueno <liseidybueno@gmail.com>
2020-08-12 20:15:08 -0400

    Update page again

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
           My awesome GitHub page running using React.
         </p>
         <a
